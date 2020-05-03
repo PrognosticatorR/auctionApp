a3d43b635679765584a577784b830c8ba82a66a1
deveshrawat <devsrawt@gmail.com>
2020-05-03 16:04:48 +0530

    setting dapp initial project

---
+++ contracts/ERC721/ERC721.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.4;
+import "./ERC721basic.sol";
+
+/**
+ * @title ERC721 Non-Fungible Token Standard basic interface
+ * @dev see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+ */
+
+contract ERC721Basic {
+    event 
+}
---
+++ contracts/ERC721/ERC721Contract.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.6.4;
+import "./ERC721.sol";
+import "./DeprecatedERC721.sol";
+import "./ERC721BasicToken.spl";
