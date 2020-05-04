59003a879c51e883f13bbafe91da363f95eb7e95
deveshrawat <devsrawt@gmail.com>
2020-05-04 19:24:16 +0530

    Updating Truffle config

--- backend/migrations/2_deploy_contract.js
+++ backend/migrations/2_deploy_contract.js
@@ -1,5 +1,5 @@
-var Auction = artifacts.require("./Auction.sol");
-var Deed = artifacts.require("./Deed.sol");
+var Auction = artifacts.require("../contracts/AuctionContract.sol");
+var Deed = artifacts.require("../contracts/Deed.sol");
 // Deed => 0xbb55adc67f64d1e6f08ba7523ecd2eca2ee434a3
 module.exports = function (deployer) {
     deployer.deploy(Auction);
